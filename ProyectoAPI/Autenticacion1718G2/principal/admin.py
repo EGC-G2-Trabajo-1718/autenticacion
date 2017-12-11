f11a5c0347bea22d979a474b2c2b6bd037902ab7
Jose Carlos <josecarlos.jcgr24@gmail.com>
2017-12-11 19:09:51 +0100

    fix: Arreglo del modelo de usuario
    El modelo de usuario no funcionaba correctamente tomando como una clase Abstract. Ahora lo toma bien habiendo realizado una relación OneToOne del User proporcionado por Django a una clase de nuestro modelo llamada Usuario.

--- ProyectoAPI/Autenticacion1718G2/principal/admin.py
+++ ProyectoAPI/Autenticacion1718G2/principal/admin.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from principal.models import Usuario
+
 from django.contrib import admin
 
 # Register your models here.
+
+# admin.site.register(Movie, MovieAdmin)
+# admin.site.register(Gendre, GendreAdmin) 
+admin.site.register(Usuario)
