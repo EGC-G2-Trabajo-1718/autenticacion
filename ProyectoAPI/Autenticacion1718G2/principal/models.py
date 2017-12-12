ea4ef2bcb817cf33275fdba3bc24739c867a6f49
Tania <tanitasa96@gmail.com>
2017-12-12 11:10:55 +0100

    feat: añadidas comunidades al modelo
    Se han añadido las comunidades autonomas para seleccionarlas desde un listado en el modelo Usuario.

--- ProyectoAPI/Autenticacion1718G2/principal/models.py
+++ ProyectoAPI/Autenticacion1718G2/principal/models.py
@@ -8,6 +8,7 @@ from django.contrib.auth.models import User
 
 ROLES=(('ASISTENTE','asistente'),('PONENTE',"ponente"),('AMBOS','ambos'))
 GENEROS=(('MASCULINO','masculino'),('FEMENINO',"femenino"))
+COMUNIDADES=(('ANDALUCIA','Andalucia'),('ARAGON','Aragon'),('ASTURIAS','Asturias'),('CANTABRIA','Cantabria'),('CASTILLA LA MANCHA','Castilla la mancha'),('CASTILLA LEON','Castilla leon'),('EXTREMADURA','Extremadura'),('GALICIA','Galicia'),('LA RIOJA','La rioja'),('MADRID','Madrid'),('MURCIA','Murcia'),('NAVARRA','Navarra'),('PAIS VASCO','Pais vasco'),('VALENCIA','Valencia'),('BALEARES','Baleares'),('CANARIAS','Canarias'),('CEUTA','Ceuta'),('MELILLA','Melilla'))
 
 class Usuario(models.Model):
     usuario = models.OneToOneField(User, primary_key=True)
@@ -15,7 +16,7 @@ class Usuario(models.Model):
     genre = models.CharField(max_length=20,choices=GENEROS, default='-----')
     role = models.CharField(max_length=10, choices=ROLES, default='ASISTENTE')
     age = models.PositiveIntegerField()
-    autonomous_community = models.CharField(max_length=30)
+    autonomous_community = models.CharField(max_length=50,choices=COMUNIDADES, default='-----')
     name = models.CharField(max_length=50)
     surname = models.CharField(max_length=50)
     email = models.EmailField(max_length=75)
